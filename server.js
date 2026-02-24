5fbe2c8edc1b8b62cf96cb5f09c7788e878ede4a
yecit1997 <ttorresyecit@gmail.com>
2026-02-23 19:38:45 -0500

    Creacion de la estructura inicial del proyecto

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+// Instanciamos nuestro servidor
+const app = express();
+
+// Le indicamos a nuestro servidor que utilice un formato JSON
+app.use(express.json());
+
+
+
